e263bb3056443712d86c2de86055c55a7753ed84
Natan Marcos <natanmarcos11@gmail.com>
2025-03-19 11:25:22 -0300

    desafio nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,47 +1,85 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void exibirTabuleiro(int tabuleiro[10][10]){
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
+#define CODIGO_NAVIO 3
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             // percorre a matriz e exibe o valor de cada posição
-            printf("%d ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[j][i]);
         }
         printf("\n");
     }
 }
 
-void adicionarNavio(int tabuleiro[10][10], int posX, int posY, int orientacao){
-    int codigoNavio = 3; // 3 representa o navio no tabuleiro
-    int tamanho = 3; // Tamanho do navio
-    if (orientacao == 0) // orientacao do navio e horizontal
-    { 
-        for (int i = 0; i < tamanho; i++){
-            tabuleiro[posX][posY + i] = codigoNavio;
-        }
-    } 
-    else // orientacao do navio e vertical
-    { 
-        for (int i = 0; i < tamanho; i++){
-            tabuleiro[posX + i][posY] = codigoNavio;
+void adicionarNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int posX, int posY, int orientacao){
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        if (orientacao == 0) // navio horizontal
+        { 
+            tabuleiro[posX + i][posY] = CODIGO_NAVIO;
+        } 
+        else // navio vertical
+        { 
+            tabuleiro[posX][posY + i] = CODIGO_NAVIO;
         }
     }
 }
 
-bool verificarPosicaoValida(int tabuleiro[10][10], int posX, int posY, int orientacao){
-    int tamanho = 3; // Tamanho do navio
-    if (orientacao == 0) // orientacao do navio horizontal
-    { 
-        for (int i = 0; i < tamanho; i++){
-            if (tabuleiro[posX][posY + i] != 0){
+bool posicaoExiste(int posX, int posY){
+    return posX >= 0 && posX < TAMANHO_TABULEIRO && posY >= 0 && posY < TAMANHO_TABULEIRO; // verifica se a posição existe no tabuleiro
+}
+
+bool verificarPosicaoValidaNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int posX, int posY, int orientacao){
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        if (orientacao == 0) // navio horizontal
+        { 
+            if (!posicaoExiste(posX + i, posY) || tabuleiro[posX + i][posY] != 0) // verifica se a posição existe e se a posição está vazia
+            {
+                return false;
+            }
+        } 
+        else // navio vertical
+        { 
+            if (!posicaoExiste(posX, posY + i) || tabuleiro[posX][posY + i] != 0) // verifica se a posição existe e se a posição está vazia
+            {
                 return false;
             }
         }
-    } 
-    else // orientacao do navio vertical
-    { 
-        for (int i = 0; i < tamanho; i++){
-            if (tabuleiro[posX + i][posY] != 0){
+    }
+    return true;
+}
+
+void adicionarNavioDiagonal(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int posX, int posY, int orientacao){
+    // orientacao 0 == para baixo e para a direita
+    // orientacao 1 == para cima e para a esquerda
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        if (orientacao == 0) // orientacao do navio e para baixo e para a direita
+        { 
+            tabuleiro[posX + i][posY + i] = CODIGO_NAVIO;
+        } 
+        else // orientacao do navio e para cima e para a esquerda
+        { 
+            tabuleiro[posX - i][posY - i] = CODIGO_NAVIO;
+        }
+    }
+}
+
+bool verificarPosicaoValidaNavioDiagonal(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int posX, int posY, int orientacao){
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        if (orientacao == 0) // orientacao do navio e para baixo e para a direita
+        { 
+            if (!posicaoExiste(posX + i, posY + i) || tabuleiro[posX + i][posY + i] != 0) // verifica se a posição existe e se a posição está vazia
+            {
+                return false;
+            }
+        } 
+        else // orientacao do navio e para cima e para a esquerda 
+        { 
+            if (!posicaoExiste(posX - i, posY - i) || tabuleiro[posX - i][posY - i] != 0) // verifica se a posição existe e se a posição está vazia
+            {
                 return false;
             }
         }
@@ -51,41 +89,25 @@ bool verificarPosicaoValida(int tabuleiro[10][10], int posX, int posY, int orien
 
 int main() {
 
-    int tabuleiro[10][10] = {0};
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0}; // inicializa o tabuleiro com 0
 
-    bool posicaoValidaNavio1 = verificarPosicaoValida(tabuleiro, 0, 0, 0); // verficar se a posicao e valida e salva o resultado na variavel
+    // navio horizontal
+    bool posicaoValidaNavio1 = verificarPosicaoValidaNavio(tabuleiro, 0, 0, 0); // verficar se a posicao e valida e salva o resultado na variavel
     posicaoValidaNavio1 ? adicionarNavio(tabuleiro, 0, 0, 0) : printf("Posicao invalida\n"); // se a posicao for valida adiciona o navio no tabuleiro
     
-    bool posicaoValidaNavio2 = verificarPosicaoValida(tabuleiro, 2, 5, 1); // verficar se a posicao e valida e salva o resultado na variavel
-    posicaoValidaNavio2 ? adicionarNavio(tabuleiro, 2, 5, 1) : printf("Posicao invalida\n"); // se a posicao for valida adiciona o navio no tabuleiro
+    // navio vertical
+    bool posicaoValidaNavio2 = verificarPosicaoValidaNavio(tabuleiro, 3, 3, 1); // verficar se a posicao e valida e salva o resultado na variavel
+    posicaoValidaNavio2 ? adicionarNavio(tabuleiro, 3, 3, 1) : printf("Posicao invalida\n"); // se a posicao for valida adiciona o navio no tabuleiro
+
+    // navio diagonal 1
+    bool posicaoValidaNavio3 = verificarPosicaoValidaNavioDiagonal(tabuleiro, 7, 7, 0); // verficar se a posicao e valida e salva o resultado na variavel
+    posicaoValidaNavio3 ? adicionarNavioDiagonal(tabuleiro, 7, 7, 0) : printf("Posicao invalida\n"); // se a posicao for valida adiciona o navio no tabuleiro
+
+    // navio diagonal 2
+    bool posicaoValidaNavio4 = verificarPosicaoValidaNavioDiagonal(tabuleiro, 3, 8, 1); // verficar se a posicao e valida e salva o resultado na variavel
+    posicaoValidaNavio4 ? adicionarNavioDiagonal(tabuleiro, 3, 8, 1) : printf("Posicao invalida\n"); // se a posicao for valida adiciona o navio no tabuleiro
 
     exibirTabuleiro(tabuleiro); // exibe o tabuleiro
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
